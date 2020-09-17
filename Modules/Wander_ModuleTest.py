9a8c08c658f6e633fd94dddb57791e34878f2d13
jaeseo-park <geeryu12@gmail.com>
2020-09-17 08:49:33 +0000

    Modifiy wander module

--- Modules/Wander_ModuleTest.py
+++ Modules/Wander_ModuleTest.py
@@ -10,26 +10,30 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Please check your model")
     parser.add_argument("--json_seq_path", type=str, default=os.path.join(os.getcwd(),"data","tracking"), help="sample json file path")
 
-    try:
-        opt = parser.parse_known_args()[0]
+    # try:
+    opt = parser.parse_known_args()[0]
 
+    
+    model = WanderDetection(0)
+
+    file_list = os.listdir(opt.json_seq_path)
+    file_list.sort()
+    # print ("file_list: {}".format(file_list))
+    with open("wander_0625_result.json", "w") as json_file:
+        result =[]
         
-        model = WanderDetection()
-
-        file_list = os.listdir(opt.json_seq_path)
-        file_list.sort()
-        # print ("file_list: {}".format(file_list))
-
+        
         for f in file_list:
             t=os.path.join(opt.json_seq_path,f)
             frame_num = f[:-5] # json file name number
-            # print(t)
+            print(frame_num)
             with open(t) as od_result_file:
                 od_result = json.load(od_result_file)
-                print(model.analysis_from_json(od_result,frame_num))
+                result.append(model.analysis_from_json(od_result))
                 # print(model.analysis_from_json(od_result).frame)
+        json.dump(result, json_file)
 
-    except:
-        print("")
-        parser.print_help()
-        sys.exit(0)
+    # except:
+    #     print("")
+    #     parser.print_help()
+    #     sys.exit(0)
