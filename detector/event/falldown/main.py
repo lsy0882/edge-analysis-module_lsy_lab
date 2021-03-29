40196b64f98a91d942f5edd42b07f60e37037a65
Daehyeon <kdh4672@naver.com>
2021-03-29 08:25:24 +0000

    module command change

--- detector/event/falldown/main.py
+++ detector/event/falldown/main.py
@@ -5,7 +5,8 @@ import numpy as np
 import json
 import time
 from detector.event.template.main import Event
-# Notice
+
+## Notice
 # - Dummy class는 참고 및 테스트용이기 때문에 해당 class는 수정 또는 삭제하지 말고 참고만 해주시기 바랍니다.
 # - 이미 정의된 함수 및 클래스 멤버 변수의 이름은 *****절대로**** 변경하지마세요.
 
