b6765bf6f5b3a3ba7f105442a5a01a9dbb84eaf8
DESKTOP-MMLAB\JinhaSong <the_jn@naver.com>
2021-04-07 20:04:34 +0900

    Fixed bug

--- extract_results.py
+++ extract_results.py
@@ -11,7 +11,7 @@ from utils import PrintLog
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--video_path", type=str, default="videos/1_360p.mp4", help="Video path")
-    parser.add_argument("--fps", type=int, default=7, help="FPS of extraction frame ")
+    parser.add_argument("--fps", type=int, default=30, help="FPS of extraction frame ")
     parser.add_argument("--model_name", type=str, default="yolov4-416", help="Model name")
     parser.add_argument("--score_threshold", type=float, default=0.5, help="Model name")
     parser.add_argument("--nms_threshold", type=float, default=0.3, help="Model name")
@@ -52,38 +52,39 @@ if __name__ == '__main__':
         os.makedirs(frame_dir)
     if not os.path.exists(json_dir):
         os.makedirs(json_dir)
-    frame_number = 0
-    extract_frame_number = 0
-    while True:
-        ret, frame = capture.read()
+    frame_number = 1
 
-        if ret:
-            if ((frame_number % fps) + 1) % (fps/extract_fps) == 0 :
-                frame_name = "{0:06d}".format(frame_number + 1)
-                cv2.imwrite(os.path.join(frame_dir, frame_name + ".jpg"), frame)
+    frame_path_list = list()
+    try:
+        command = "ffmpeg -y -hide_banner -loglevel panic -i {} -vsync 2 -q:v 0 -vf fps={} {}/%04d.jpg".format(video_path, extract_fps, os.path.join(frame_dir))
+        os.system(command)
+        frame_path_list = os.listdir(frame_dir)
+        PrintLog.i("Frame extraction is successfully completed(path: {}, framecount: {})".format(frame_dir, len(frame_path_list)))
+    except:
+        PrintLog.e("Frame extraction is failed")
+        exit(0)
 
-                start_time = time.time()
-                results = model.inference_by_image(frame)
-                end_time = time.time()
+    for i, frame_name in enumerate(frame_path_list):
+        frame = cv2.imread(os.path.join(frame_dir, frame_name))
 
-                dict_result = dict()
-                dict_result["image_path"] = "./data/frames/1_360p/" + frame_name + ".jpg"
-                dict_result["module"] = model_name
-                dict_result["cam_id"] = 0
-                dict_result["analysis_time"] = end_time - start_time
-                dict_result["frame_number"] = frame_number
-                dict_result["results"] = results
+        start_time = time.time()
+        results = model.inference_by_image(frame)
+        end_time = time.time()
 
-                json_result_file = open(os.path.join(json_dir, frame_name + ".json"), "w")
-                json.dump(dict_result,json_result_file, indent=4)
-                json_result_file.close()
+        dict_result = dict()
+        dict_result["image_path"] = os.path.join(frame_dir, frame_name)
+        dict_result["module"] = model_name
+        dict_result["cam_id"] = 0
+        dict_result["analysis_time"] = end_time - start_time
+        dict_result["frame_number"] = frame_number
+        dict_result["results"] = results
 
-                print("\rframe number: {:>6}/{}".format(frame_number, frame_count), end='')
-                extract_frame_number += 1
+        json_result_file = open(os.path.join(json_dir, frame_name.split(".jpg")[0] + ".json"), "w")
+        json.dump(dict_result,json_result_file, indent=4)
+        json_result_file.close()
 
-            frame_number += 1
-        else:
-            print()
-            break
+        print("\rframe number: {:>6}/{}".format(frame_number, len(frame_path_list)), end='')
+        frame_number += 1
 
-    PrintLog.i("Extraction is successfully completed(framecount: {})".format(extract_frame_number))
+    print()
+    PrintLog.i("Extraction is successfully completed(framecount: {})".format(frame_number))
