771907a3b89994eee143addafd4f4185c3b8539c
wonkicho <chowk1109@naver.com>
2022-05-20 14:51:11 +0900

    update tailing module

--- detector/event/tailing/main.py
+++ detector/event/tailing/main.py
@@ -1,43 +1,27 @@
-from collections import OrderedDict
-
 import os
 import cv2
 import json
 import itertools
 import similaritymeasures
+from collections import OrderedDict, defaultdict
+
+
 import time
 import numpy as np
+import pandas as pd
 import math
 from itertools import combinations
-from detector.event.template.main import Event
+from scipy.spatial.distance import euclidean
 
+from detector.event.tailing.utils import *
+from detector.event.template.main import Event
+from detector.tracker.tailing_tracker.BYTETracker import BYTETracker
+
+tracker = BYTETracker(track_thresh=0.5, track_buffer=30, match_thresh=0.8, min_box_area=10, frame_rate=20)
 
 # Notice
 # - Dummy class는 참고 및 테스트용이기 때문에 해당 class는 수정 또는 삭제하지 말고 참고만 해주시기 바랍니다.
 # - 이미 정의된 함수 및 클래스 멤버 변수의 이름은 *****절대로**** 변경하지마세요.
-def cos_sim(v1_1, v1_2, v2_1, v2_2):
-    vector_1 = v1_2 - v1_1
-    vector_2 = v2_2 - v2_1
-    if vector_1[0] == 0.0:
-        vector_1[0] = 0.01
-
-    if vector_1[1] == 0.0:
-        vector_1[1] = 0.01
-
-    if vector_2[0] == 0.0:
-        vector_2[0] = 0.01
-
-    if vector_2[1] == 0.0:
-        vector_2[1] = 0.01
-
-    return np.dot(vector_1, vector_2) / (np.linalg.norm(vector_1) * np.linalg.norm(vector_2))
-
-def check_zeroValue(coordinates, n):
-    if coordinates[n][0] == 0.0 or coordinates[n][1] == 0.0:
-        return check_zeroValue(coordinates, n - 1)
-    else:
-        return coordinates[n]
-
 
 class TailingEvent(Event):
     model = None
@@ -49,22 +33,23 @@ class TailingEvent(Event):
         self.model_name = "tailing"
         self.analysis_time = 0
         self.debug = debug
-        self.history = [0, ] * 10
+        self.history = [0, ] * 40
         self.result = False
 
-        self.frechet_dl = np.zeros(shape=(11,11, 2), dtype=np.float32)
-        self.max_history = 10
+        
+        self.max_history = 40
         self.frame = None
         self.frame_cnt = 0
         self.row_idx = 0
-        self.prev_person = 0
-        self.prev_sim = 0.0
-
-        self.mask = None
-        self.prev_mask = None
-        self.prev_frame = None
         self.nonmove_cnt = 0
 
+       
+        self.prev_stack = None
+        self.id_dict_stack = {}
+        self.id_cp_dict = defaultdict(list)
+        self.prev_track_id_list = []
+        self.dict = defaultdict(list)
+        self.frame_raise = []
         self.smoothBox = []
 
         # TODO: __init__
@@ -74,126 +59,129 @@ class TailingEvent(Event):
 
     def inference(self, frame_info, detection_result, score_threshold=0.5):
         detection_result = self.filter_object_result(detection_result, score_threshold)
+        #h,w = frame_info['frame'].shape[0], frame_info['frame'].shape[1]
         start = 0
         end = 0
+        self.frame_cnt += 1
         if self.debug:
             start = time.time()
 
         result = OrderedDict()
-        detected_person = []
-        person_bbox = []
-        for i, e in enumerate(detection_result['results'][0]["detection_result"]):
-            if e['label'][0]['description'] in ['person'] and e['label'][0]['score'] >= 0.60 and (e['position']['w'] < e['position']['h']) and (e['position']['w'] / e['position']['h'])<0.9:
-                
-                detected_person.append(
-                    ((e['position']['x'] + e['position']['w'] / 2), (e['position']['y'] + e['position']['h'] / 2)))
-                person_bbox.append([e['position']['x'], e['position']['y'], e['position']['w'], e['position']['h']])
-
-        num_of_person = len(detected_person)
-        result["num_of_person"] = num_of_person
-        result["center_coordinates"] = detected_person
-
-        # tailing detection module
-        # 시작 5frame은 0인 상태
-        eventFlag = [0, ] * 10
-        tmp = 0
-        result["center_coordinates"] = sorted(result["center_coordinates"])
         
-        if self.frame != None:
-            if (result["num_of_person"] < 2 and result["num_of_person"] >=4) and (self.frame["num_of_person"] < 2 and self.frame["num_of_person"] >=4):
-                self.frechet_dl = np.zeros(shape=(11, 11, 2), dtype=np.float32)
-                self.max_history = 10
-                self.frame = None
-                self.result = False
-                self.frame_cnt = 0
-                self.nonmove_cnt = 0
-                self.row_idx = 0
-                eventFlag = [0, ] * 10
-                self.prev_person = 0
+        boxes_list_array, scores_list_array, classes_list_array = det_to_trk_data_conversion(detection_result)
+        tracked_stracks_history = tracker.update(boxes_list_array, scores_list_array, classes_list_array)
 
-            elif 4 > result["num_of_person"] >= 2:
-                self.frechet_dl[self.row_idx, :len(result["center_coordinates"])] = result["center_coordinates"]
-                if self.row_idx >= 10:
-                    self.row_idx = 0
+        eventFlag = [0, ] * 40
+        
+        if 2 <= len(tracked_stracks_history):
+            human_stracks = [strack for strack in tracked_stracks_history[-1]['tracked_stracks_list'] if strack.cls == 0 and is_not_boundary(strack.xyah) == True and 800 < strack.tlwh[2] * strack.tlwh[3]]
+            for human_strack in human_stracks:
+                
+                track_id = human_strack.track_id
+                human_coord = human_strack.xyah[:2]
+
+                if  track_id in self.id_dict_stack:
+                    self.id_dict_stack[track_id] += 1
                 else:
-                    self.row_idx += 1
+                    self.id_dict_stack[track_id] = 1
+                
+                
+                if track_id in self.id_cp_dict:
+                    self.id_cp_dict[track_id].append(human_coord)
+                else:
+                    self.id_cp_dict[track_id] = []
 
-                if self.frame_cnt >= 10:
-                    each_person_cordinates = []  # 검출된 사람마다의 각 이동좌표를 하나의 열로 연결
-                    combi_frechet = []
+                
+        if self.frame_cnt >= 40:
+            
+            temp = []
+            temp_dict = {}
+            for k, v in self.id_dict_stack.items(): #id, stack count
+                if v > 5:  
+                    temp.append(k)
 
-                    r_list, c_list = [], []
-                    for i in range(len(self.frechet_dl)):
-                        if np.sum(self.frechet_dl[i]) == 0.0:
-                            r_list.append(i)
-                        if np.sum(self.frechet_dl[:, i]) == 0.0:
-                            c_list.append(i)
+            if 4 > len(temp) >= 2:
+                equal_id = [t for t in temp if t in self.prev_track_id_list]
 
-                    self.frechet_dl = np.delete(self.frechet_dl, r_list, axis=0)
-                    self.frechet_dl = np.delete(self.frechet_dl, c_list, axis=1)
+                for i in equal_id:
+                    coord = np.array(self.id_cp_dict[i])
+                    temp_dict[i] = coord
+                
+                
+
+                coord_values = [v for k, v in temp_dict.items()]
+                comb_id = list(combinations(equal_id, 2))
+
+                comb_coords = list(combinations(coord_values, 2))
+                dist_compared = np.array(
+                    [np.linalg.norm(comb_coords[i][0][-1] - comb_coords[i][1][-1]) for i in range(len(comb_coords))])
+                
+                combi_frechet = np.array(
+                            [similaritymeasures.frechet_dist(comb_coords[i][0], comb_coords[i][1]) for i in
+                                range(len(comb_coords))])
+
+
+                del_idx = []
+                for i in range(len(comb_id)):
+                    if (dist_compared[i] > 350 or dist_compared[i] < 60) or (dist_compared[i] < combi_frechet[i]):
+                        del_idx.append(i)
+
+                
+                if del_idx:
+                    dist_compared = np.delete(dist_compared, del_idx)
+                    combi_frechet = np.delete(combi_frechet, del_idx)
                     
+                
+                del_person_id = []
+                for i in del_idx:
+                    del_person_id.append(comb_id[i])
+                for i in del_person_id:
+                    if i in comb_id:
+                        comb_id.remove(i)
+                
+
+                if len(combi_frechet) > 0 and len(dist_compared)>0:
+                    target_comb = comb_id[np.argmin(combi_frechet)]
+                    first_pid = target_comb[0]
+                    second_pid = target_comb[1]
+
+                    f_sp, f_ep = temp_dict[first_pid][0], temp_dict[first_pid][-1]
+                    s_sp, s_ep = temp_dict[second_pid][0], temp_dict[second_pid][-1]
                     
-                    if 4 > self.frechet_dl.shape[1] >= 2:
-                        transpose_frechet_dl = list(map(list, zip(*self.frechet_dl)))
-                        each_person_comb = np.array(list(combinations(transpose_frechet_dl, 2)), dtype=float)
-                        combi_frechet = np.array(
-                            [similaritymeasures.frechet_dist(each_person_comb[i][0], each_person_comb[i][1]) for i in
-                             range(len(each_person_comb))])
-                        
-                        if (combi_frechet.size == 0) is False:
-                            min_idx = np.argmin(combi_frechet)
-
-                            # 각 사람 start point, end point
-                            ln = len(each_person_comb[min_idx][0]) - 1
-                            p1_sp, p1_ep = each_person_comb[min_idx][0][0], check_zeroValue(
-                                each_person_comb[min_idx][0], ln)
-                            p2_sp, p2_ep = each_person_comb[min_idx][1][0], check_zeroValue(
-                                each_person_comb[min_idx][1], ln)
-
-                            
-                            dist = np.linalg.norm(p1_ep - p2_ep)
-                            
-                            movement = abs(np.linalg.norm(p1_ep - p1_sp) - np.linalg.norm(p2_ep - p2_sp))
-                            compare_simiarity = abs(combi_frechet[min_idx] - dist)
-                            
-                           
-                            if (1.0 > cos_sim(p1_sp, p1_ep, p2_sp, p2_ep) > 0.3) and 270.0 > dist > 80.0 and 0.0 < abs(compare_simiarity - movement)<10: 
-                            
-                                eventFlag = [1, ] * 10
-                                
-                            else:
-                                eventFlag = [0, ] * 10
-                            self.prev_person = result["num_of_person"]
+                    if 0.6 < cos_sim(f_ep, f_sp, s_ep, s_sp) < 1.0:
+                        eventFlag = [1, ] * 40
                     else:
-                        eventFlag = [0, ] * 10
+                        eventFlag = [0, ] * 40
+            else:
+                eventFlag = [0, ] * 40
 
-                    
-                    self.frechet_dl = np.zeros(shape=(11, 11, 2), dtype=np.float32)
-                    self.frame_cnt = 0
-                    
-                    self.history.extend(eventFlag)
-                    if len(self.history) >= self.max_history:
-                        self.history = self.history[10:]
-                        if sum(self.history) >= 7:
-                            tmp = 1
-                        else :
-                            tmp = 0
-                    else :
-                        tmp = 0
+            self.frame_cnt = 0
+            self.id_dict_stack = {}
+            self.prev_track_id_list = temp
+                           
+
+        self.history.extend(eventFlag)
+        if len(self.history) >= self.max_history:
+            self.history = self.history[40:]
+            if sum(self.history) >= 25:
+                tmp = 1
+            else :
+                tmp = 0
         else :
             tmp = 0
-
+        
+        
         if len(self.smoothBox) == 200:
             self.smoothBox.pop(0)
         self.smoothBox.append(tmp)                            
 
         
-        if sum(self.smoothBox) >= 3:
+        if sum(self.smoothBox) >= 2:
             self.result = True
         else :
             self.result = False
         
-        self.frame_cnt += 1
+        
         self.frame = result
 
         # TODO: analysis(끝 지점)
