54ef08687549adcad446669c06d4e2fce5453b51
DESKTOP-MMLAB-O\JinhaSong <jinhasong@sogang.ac.kr>
2022-08-07 21:11:00 +0900

    Update config/config.py - object detection model, tracker, event model에 대한 dict 추가

--- config/config.py
+++ config/config.py
@@ -1,2 +1,27 @@
+from detector.object_detection.yolov4.yolov4 import YOLOv4
+from detector.tracker.byte_tracker.BYTETracker import BYTETracker
+from detector.tracker.sort.Sort import Sort
+from detector.event.assault.main import AssaultEvent
+from detector.event.falldown.main import FalldownEvent
+from detector.event.kidnapping.main import KidnappingEvent
+from detector.event.tailing.main import TailingEvent
+from detector.event.wanderer.main import WandererEvent
+
+
 DEBUG = True
 SETTINGS_PATH = "config/settings.yml"
+
+OBJECT_MODEL = {
+    "yolov4-416": YOLOv4
+}
+TRACKER = {
+    "byte_tracker": BYTETracker,
+    "sort_tracker": Sort
+}
+EVENT_MODEL = {
+    "assault": AssaultEvent,
+    "falldown": FalldownEvent,
+    "kidnapping": KidnappingEvent,
+    "tailing": TailingEvent,
+    "wanderer": WandererEvent,
+}
